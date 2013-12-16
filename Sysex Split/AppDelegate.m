c8eebc9278f24b6183e1541c8a8208cfc708ebcf
# <#>
2013-12-16 17:42:08 +0100

    removed superfluous sysex IDs

--- Sysex Split/AppDelegate.m
+++ Sysex Split/AppDelegate.m
@@ -16,13 +16,6 @@ typedef enum A4SysexMessageID
 	A4SysexMessageID_Song		= 0x55,
 	A4SysexMessageID_Settings	= 0x56,
 	A4SysexMessageID_Global		= 0x57,
-	
-	A4SysexMessageID_Kit_X		= 0x58,
-	A4SysexMessageID_Sound_X	= 0x59,
-	A4SysexMessageID_Pattern_X	= 0x5A,
-	A4SysexMessageID_Song_X		= 0x5B,
-	A4SysexMessageID_Settings_X	= 0x5C,
-	A4SysexMessageID_Global_X	= 0x5D,
 }
 A4SysexMessageID;
 
